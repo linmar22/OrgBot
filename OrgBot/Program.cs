373fe0b123535ad14771d3702b4a73dd4a7c5e45
Linas Martusevicius <linas.martusevicius@gmail.com>
2018-02-04 05:32:52 +0100

    Solution Init

---
+++ OrgBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace OrgBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            string token = "abcdefg..."; // Remember to keep this private!
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ OrgBot/SettingsProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+// Requires NuGet package
+// Microsoft.Extensions.Configuration.Json
+using Microsoft.Extensions.Configuration;
+
+namespace OrgBot
+{
+    public class SettingsProvider
+    {
+        private static IConfiguration _conf { get; set; }
+
+        public SettingsProvider()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            _conf = builder.Build();
+        }
+
+        
+
+        public string GetClientId() {
+            return _conf["ClientId"];
+        }
+
+        public string GetClientSecret()
+        {
+            return _conf["ClientSecret"];
+        }
+    }
+}
